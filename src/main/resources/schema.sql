3c19eda281a9cbba356b398cd08103e3fe908d0a
TheMechanist001 <priti_bhowmik@live.com>
2020-09-23 00:51:02 -0500

    Runnable App. Added schema script, added javassist dependency to work with current Java version.

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -2,6 +2,8 @@ DROP TABLE IF EXISTS DRIVER;
 
 CREATE TABLE DRIVER (
   id INT AUTO_INCREMENT  PRIMARY KEY,
+  coordinate VARCHAR(250),
+  date_coordinate_updated DATE,
   date_created DATE NOT NULL,
   deleted BOOLEAN NOT NULL,
   online_status VARCHAR(250) NOT NULL,

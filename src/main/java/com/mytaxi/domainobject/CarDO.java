0789c0ecebfb6a12ea3f332dc7c3cb2730246d83
TheMechanist001 <priti_bhowmik@live.com>
2020-09-25 22:29:09 -0500

    Runnable App. Car CRUD in progress.

--- src/main/java/com/mytaxi/domainobject/CarDO.java
+++ src/main/java/com/mytaxi/domainobject/CarDO.java
@@ -1,6 +1,74 @@
 package com.mytaxi.domainobject;
 
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
 public class CarDO {
 
+    private UUID carId;
+    private ZonedDateTime dateCreated;
+    private String licensePlate;
+    private int seatCount;
+    private boolean convertible;
+    private long rating;
+    private String engineType;
+    private String manufacturer;
+    private long selectedDriverId;
+    private ZonedDateTime dateSelected;
 
+    public UUID getCarId() {
+        return carId;
+    }
+
+    public String getLicensePlate() {
+        return licensePlate;
+    }
+
+    public int getSeatCount() {
+        return seatCount;
+    }
+
+    public boolean isConvertible() {
+        return convertible;
+    }
+
+    public long getRating() {
+        return rating;
+    }
+
+    public String getEngineType() {
+        return engineType;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public long getSelectedDriverId() {
+        return selectedDriverId;
+    }
+
+    public ZonedDateTime getDateCreated() {
+        return dateCreated;
+    }
+
+    public ZonedDateTime getDateSelected() {
+        return dateSelected;
+    }
+
+    public CarDO() {
+    }
+
+    public CarDO(UUID carId, ZonedDateTime dateCreated, String licensePlate, int seatCount, boolean convertible, long rating, String engineType, String manufacturer, long selectedDriverId, ZonedDateTime dateSelected) {
+        this.carId = carId;
+        this.dateCreated = dateCreated;
+        this.licensePlate = licensePlate;
+        this.seatCount = seatCount;
+        this.convertible = convertible;
+        this.rating = rating;
+        this.engineType = engineType;
+        this.manufacturer = manufacturer;
+        this.selectedDriverId = selectedDriverId;
+        this.dateSelected = dateSelected;
+    }
 }
